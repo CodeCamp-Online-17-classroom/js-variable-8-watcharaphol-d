77539aadde00318688efd8fcb2b4d68fbdea923c
watcharaphol.d <dangpiem@hotmail.com>
2024-06-21 23:14:53 +0700

    Change the variable value with Math operation

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // write code here
+let a = 0;
+a++;        // Increment
+a += 3;     // Modify-in-place
+a *= 17;    // Modify-in-place
+a %= 7      // Modify-in-place
+console.log(`The value of variable 'a' is: ${a}`); // The result of 'a' is: 5
